84a86484277dab6ebe16f29df44046f7a9f1348e
Mara Hart <maralihart@gmail.com>
2021-04-30 13:12:05 -0400

    attempted api

--- env-guide.md
+++ env-guide.md
@@ -24,3 +24,8 @@ useEffect(() => {
     fetchItems();
   }, []);
 ```
+
+curl --location --request \
+GET 'https://api.twitch.tv/helix/search/channels?query=MaraDrinksMilk' \
+--header 'client-id: wbmytr93xzw8zbg0p1izqyzzc5mbiz' \
+--header 'Authorization: Bearer 2gbdx6oar67tqtcmt49t3wpcgycthx'
--- src/components/Header/Twitch.tsx
+++ src/components/Header/Twitch.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import TwitchIcon from '../../assets/twitch-logo.png';
+import axios from 'axios';
 
 function Twitch() {
+  const [online, setOnline] = useState(false);
+
+  // Thanks to twitch.tv/WebsiteDesign for the help!
+  const url = 'https://api.twitch.tv/helix/search/channels?query=MaraDrinksMilk';
+  const config = {
+      headers: {
+          client_id: process.env.REACT_APP_TWITCH_CLIENT,
+          Authorization: 'Bearer ' + process.env.REACT_APP_TWITCH_SECRET
+      }
+  };
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await axios.get(url, config)
+      console.log(response.data);
+    }
+    fetchItems();
+    // setOnline(dummyVar);
+    // const fetchItems = async () => {
+    //   const { items } = await request(
+    //     process.env.REACT_APP_GRAPHCMS_ENDPOINT ? process.env.REACT_APP_GRAPHCMS_ENDPOINT : "",
+    //     `
+    //       { 
+    //         items {
+    //           values
+    //         }
+    //       }
+    //     `
+    //   );
+    //   setValue(items);
+    // };
+
+    // fetchItems();
+  }, []);
+
   return (
     <div className="my-5">
       <a className="live" href="https://www.twitch.tv/MaraDrinksMilk" target="_blank" rel="noopener noreferrer">
