136462b40760da45888894561bf8e11b7caa0332
Mara Hart <maralihart@gmail.com>
2021-05-03 23:14:26 -0400

    added favicons and search engine description

--- src/components/Header/Twitch.tsx
+++ src/components/Header/Twitch.tsx
@@ -1,43 +1,43 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import TwitchIcon from '../../assets/twitch-logo.png';
-import axios from 'axios';
+// import axios from 'axios';
 
 function Twitch() {
-  const [online, setOnline] = useState(false);
+  // const [online, setOnline] = useState(false);
 
-  // Thanks to twitch.tv/WebsiteDesign for the help!
-  const url = 'https://api.twitch.tv/helix/search/channels?query=MaraDrinksMilk';
-  const config = {
-      headers: {
-          client_id: process.env.REACT_APP_TWITCH_CLIENT,
-          Authorization: 'Bearer ' + process.env.REACT_APP_TWITCH_SECRET
-      }
-  };
+  // // Thanks to twitch.tv/WebsiteDesign for the help!
+  // const url = 'https://api.twitch.tv/helix/search/channels?query=MaraDrinksMilk';
+  // const config = {
+  //     headers: {
+  //         client_id: process.env.REACT_APP_TWITCH_CLIENT,
+  //         Authorization: 'Bearer ' + process.env.REACT_APP_TWITCH_SECRET
+  //     }
+  // };
 
-  useEffect(() => {
-    const fetchItems = async () => {
-      const response = await axios.get(url, config)
-      console.log(response.data);
-    }
-    fetchItems();
-    // setOnline(dummyVar);
-    // const fetchItems = async () => {
-    //   const { items } = await request(
-    //     process.env.REACT_APP_GRAPHCMS_ENDPOINT ? process.env.REACT_APP_GRAPHCMS_ENDPOINT : "",
-    //     `
-    //       { 
-    //         items {
-    //           values
-    //         }
-    //       }
-    //     `
-    //   );
-    //   setValue(items);
-    // };
+  // useEffect(() => {
+  //   const fetchItems = async () => {
+  //     const response = await axios.get(url, config)
+  //     console.log(response.data);
+  //   }
+  //   fetchItems();
+  //   // setOnline(dummyVar);
+  //   // const fetchItems = async () => {
+  //   //   const { items } = await request(
+  //   //     process.env.REACT_APP_GRAPHCMS_ENDPOINT ? process.env.REACT_APP_GRAPHCMS_ENDPOINT : "",
+  //   //     `
+  //   //       { 
+  //   //         items {
+  //   //           values
+  //   //         }
+  //   //       }
+  //   //     `
+  //   //   );
+  //   //   setValue(items);
+  //   // };
 
-    // fetchItems();
-  }, []);
+  //   // fetchItems();
+  // }, []);
 
   return (
     <div className="my-5">
