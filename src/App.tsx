2e6ad331dea8fa4c2772418951cc686010cac8e3
Mara Hart <maralihart@gmail.com>
2021-06-19 22:43:14 -0700

    added links page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import About from './components/About/About';
 import Header from './components/Header/Header';
 import { isMobile } from 'react-device-detect';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Links from './components/Links/Links';
 
 function App() {
+
   return (
-    <div className={isMobile ? "mx-3" : "mx-4"}>
-      <Header />
-      <About />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/links">
+          <Links/>
+        </Route>
+        <Route path="/">
+          <div className={isMobile ? "mx-3" : "mx-4"}>
+            <Header />
+            <About />
+          </div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
