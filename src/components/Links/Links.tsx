3e9ad8e3fa9e466909f245d92bccab041e653e78
Mara Hart <maralihart@gmail.com>
2021-07-15 16:58:14 -0700

    commented out  discord

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -9,9 +9,9 @@ import TikTokIcon from '../../assets/tiktok-logo.png';
 import InstagramIcon from '../../assets/instagram-logo.png';
 
 function Links() {
-  
-  const [links, setLinks] = useState([{'title': '', 'link': '', 'id': ''}]);
-  
+
+  const [links, setLinks] = useState([{ 'title': '', 'link': '', 'id': '' }]);
+
   useEffect(() => {
     const fetchLinks = async () => {
       const { links } = await request(
@@ -35,43 +35,43 @@ function Links() {
   return (
     <Col>
       <Row className="justify-content-around pt-5 pb-3">
-        <img 
-          className="img-circle" 
+        <img
+          className="img-circle"
           src={pfp}
-          width="125px" height="125px" 
-          alt="Mara drinking juice at brunch"/>
+          width="125px" height="125px"
+          alt="Mara drinking juice at brunch" />
       </Row>
       <h2 className="text-center">@MaraDrinksMilk</h2>
       <p className="text-center">👋 Hi, I'm Mara!</p>
       <Row className="justify-content-around my-2 faded-bg py-3 rounded">
         <Col>
           <a href="https://twitch.tv/MaraDrinksMilk" target="_blank" rel="noreferrer noopener">
-            <img src={TwitchIcon} width="28px" alt="Twitch logo"/>
+            <img src={TwitchIcon} width="28px" alt="Twitch logo" />
           </a>
         </Col>
-        <Col>
+        {/* <Col>
           <a href="https://discord.gg/ebMsPKpkBu" target="_blank" rel="noreferrer noopener">
             <img src={DiscordIcon} width="28px" alt="Discord logo"/>
           </a>
-        </Col>
+        </Col> */}
         <Col>
           <a href="https://instagram.com/mi.lk" target="_blank" rel="noreferrer noopener">
-            <img src={InstagramIcon} width="28px" alt="Instagram logo"/>
+            <img src={InstagramIcon} width="28px" alt="Instagram logo" />
           </a>
         </Col>
         <Col>
           <a href="https://tiktok.com/@maradrinksmilk" target="_blank" rel="noreferrer noopener">
-            <img src={TikTokIcon} width="28px" alt="Tiktok logo"/>
+            <img src={TikTokIcon} width="28px" alt="Tiktok logo" />
           </a>
         </Col>
         <Col>
           <a href="https://twitter.com/maradrinksmilk" target="_blank" rel="noreferrer noopener">
-            <img src={TwitterIcon} width="28px" alt="Twitter logo"/>
+            <img src={TwitterIcon} width="28px" alt="Twitter logo" />
           </a>
         </Col>
       </Row>
       <Col>
-        {links.map((item) => 
+        {links.map((item) =>
           <Row className="mt-3 mx-0" key={item.id}>
             <Button className="py-3 button full-width smaller-text" href={item.link} target="_ blank" block>{item.title}</Button>
           </Row>)}
