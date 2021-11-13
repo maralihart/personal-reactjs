e282363c72903648aab9bea8edebac9dbddd82cd
Mara Hart <maralihart@gmail.com>
2021-11-13 00:19:25 -0500

    affiliate link info

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -22,6 +22,7 @@ export default function Links() {
               link
               title
               discount
+              affiliate
             }
           }
         `
@@ -32,35 +33,42 @@ export default function Links() {
 
   return (
     <>
-      <Row style={{
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        margin: "20px 0 7px 0"
-      }}>
-        <a href={Instagram} target="_blank" rel="noreferrer noopener">
-          <img src={InstagramIcon} width={isMobile ? "40px" : "80px"} alt="Instagram logo" />
-        </a>
-        <img
-          className="circle"
-          src={pfp}
-          width="125px" height="125px"
-          alt="Mara drinking juice at brunch" />
-        <a href={TikTok} target="_blank" rel="noreferrer noopener">
-          <img src={TikTokIcon} width={isMobile ? "35px" : "75px"} alt="Tiktok logo" />
-        </a>
-      </Row>
-      <h1 className="text-center" style={{ fontFamily: "Source Sans Pro", textTransform: "uppercase" }}>mara</h1>
-      {/* <h2 className="text-center">assalamu alaiykum</h2> */}
-      <Row style={{ justifyContent: "space-evenly", alignItems: "center" }}>
-        <p className="text-center">collab: pr@mara.fyi</p>
-        <p style={{ background: secondary, padding: "10px"}}><a className="text-white" href="./MARA_HART_MEDIAKIT_PUBLIC.pdf" 
-        download="MARA_HART_MEDIAKIT">
-          download media kit</a></p>
-      </Row>
-      <Col>
-        {/* TODO: Add support button <LinkDropdownButton>support me</LinkDropdownButton> */}
-        {links.map((item) => <LinkButton link={item.link} key={item.id}>{item.title.toLowerCase()}{item.discount && ` | ${item.discount.toUpperCase()} to save`}</LinkButton>)}
-      </Col>
+      <div class="buffer">
+        <Row style={{
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          margin: "20px 0 7px 0"
+        }}>
+          <a href={Instagram} target="_blank" rel="noreferrer noopener">
+            <img src={InstagramIcon} width={isMobile ? "40px" : "80px"} alt="Instagram logo" />
+          </a>
+          <img
+            className="circle"
+            src={pfp}
+            width="125px" height="125px"
+            alt="Mara drinking juice at brunch" />
+          <a href={TikTok} target="_blank" rel="noreferrer noopener">
+            <img src={TikTokIcon} width={isMobile ? "35px" : "75px"} alt="Tiktok logo" />
+          </a>
+        </Row>
+        <h1 className="text-center" style={{ fontFamily: "Source Sans Pro", textTransform: "uppercase" }}>mara</h1>
+        {/* <h2 className="text-center">assalamu alaiykum</h2> */}
+        <Row style={{ justifyContent: "space-evenly", alignItems: "center" }}>
+          <p className="text-center">collab: pr@mara.fyi</p>
+          <p style={{ background: secondary, padding: "10px"}}><a className="text-white" href="./MARA_HART_MEDIAKIT_PUBLIC.pdf" 
+          download="MARA_HART_MEDIAKIT">
+            download media kit</a></p>
+        </Row>
+        <p style={{ fontSize: ".7rem", textAlign: "center" }}>fit details can be found in <a href="https://www.instagram.com/stories/highlights/17927917030896477/" target="_blank" rel="noreferrer noopener">Instagram story highlights</a></p>
+        <Col>
+          {/* TODO: Add support button <LinkDropdownButton>support me</LinkDropdownButton> */}
+          {links.map((item) => <LinkButton link={item.link} key={item.id}>{item.title.toLowerCase()}{item.discount && ` | ${item.discount.toUpperCase()} to save`}{item.affiliate && <p class="tiny">*</p>}</LinkButton>)}
+        </Col>
+      </div>
+      <footer>
+        <p class="tiny" style={{ fontSize: "6pt", color: "inherit"}}>* purchases made by these links earn the creator a percentage of the purchase. thank you for your support! </p>
+        <p class="tiny" style={{ fontSize: "6pt", color: "inherit"}}>website designed & coded by <a href="https://linkedin.com/in/maralihart">mara hart</a></p>
+      </footer>
     </>
   );
 }
