8769a191f2d13560be5cf44436d140ea0e709f8a
Mara Hart <maralihart@gmail.com>
2021-11-15 18:10:03 -0500

    footer added

---
+++ src/components/Links/Footer.js
@@ -0,0 +1,9 @@
+export default function LinksFooter() {
+
+  return (
+    <footer>
+      <p class="tiny" style={{ fontSize: "6pt", color: "inherit"}}>* purchases made by these links earn the creator a percentage of the purchase. thank you for your support! </p>
+      <p class="tiny" style={{ fontSize: "6pt", color: "inherit"}}>website designed & coded by <a href="https://linkedin.com/in/maralihart">mara hart</a></p>
+    </footer>
+  );
+}
